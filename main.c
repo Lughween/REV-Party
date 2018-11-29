661148d91b5947de2b1c8112f2cfbf7d688a5f53
Votre Nom <ludovic.viala@gmail.com>
2018-11-29 10:58:24 +0100

    update main.c

--- main.c
+++ main.c
@@ -1,51 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define bal_i 0
 #define bal_l 1
 #define bal_d 2
 #define bal_m 3
 
-int main(int argc, char const *argv[])
-{
-    char **param_balise; //un tableau de chaine de caractères
-    param_balise = malloc(4*sizeof(char*)); // une case par balise possible
+bool is_bal_i = false;
+bool is_bal_d = false;
+bool is_bal_l = false;
+bool is_bal_m = false;
 
-    int indice_bal_i = -1; //position pour chaque balise de son argument dans param_balise
-    int indice_bal_d = -1;
-    int indice_bal_l = -1;
-    int indice_bal_m = -1;
-    int i =-1; //compteur 
+char **analyse_balise_get_arg(int argc,char const *argv[]){
+    char **param_balise; //un tableau de chaine de caractères
+    param_balise = malloc(4*sizeof(char*)); // une case par balise possible (max 3 balises)
+
 
     int i_arg=1; //numéro argument
-    while(i_arg<argc){
-        if (argv[i_arg][0] == '-' && i_arg <argc-1 && i<3){
-            if(argv[i_arg][1] == 'i' && indice_bal_d == -1 && indice_bal_i == -1){
-                param_balise[++i] = argv[++i];
-                indice_bal_i = i;
+    while(i_arg<argc){ //pour chaque argument analyser les balises
+        if (argv[i_arg][0] == '-' && i_arg <argc-1){
+            if(argv[i_arg][1] == 'i' && !is_bal_i){
+                if(is_bal_d){
+                    puts("Balise -d et -i sont incompatibles !!\n");
+                    exit(EXIT_FAILURE);}
+                param_balise[bal_i] = argv[++i_arg];
+                is_bal_i = true;
             }
-            else if(argv[i_arg][1] == 'd' && indice_bal_d == -1 && indice_bal_i == -1)
+            else if(argv[i_arg][1] == 'd' && !is_bal_d)
             {
-                param_balise[++i] = argv[++i];
-                indice_bal_d = i;
+                if(is_bal_i){
+                    puts("Balise -d et -i sont incompatibles !!\n");
+                    exit(EXIT_FAILURE);}
+                param_balise[bal_d] = argv[++i_arg];
+                is_bal_d = true;
             }
-            else if(argv[i_arg][1] == 'l' && indice_bal_l == -1){
-                param_balise[++i] = argv[++i];
-                indice_bal_l = i;
+            else if(argv[i_arg][1] == 'l' && !is_bal_l){
+                param_balise[bal_l] = argv[++i_arg];
+                is_bal_l = true;
             }
-            else if(argv[i_arg][1] == 'm' && indice_bal_m == -1){
-                param_balise[++i] = argv[++i];
-                indice_bal_m = i;
+            else if(argv[i_arg][1] == 'm' && !is_bal_m){
+                param_balise[bal_m] = argv[++i_arg];
+                is_bal_m = true;
             }
             else
             {
-                puts("erreur argument !\n");
-                exit(EXIT_FAILURE);
+            puts("erreur argument !\n");
+            exit(EXIT_FAILURE);
             }
-            
         }
-        
-        return param_balise;
-    }    
+        else
+        {
+            puts("erreur argument !\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    return param_balise;  
+}
+
+int main(int argc, char const *argv[])
+{   
+    analyse_balise_get_arg(argc,argv);
     return 0;
 }
